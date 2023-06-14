76bf753341f3172c61cf91f66fd6521bd958ccaf
hughbrien <hugh@hughbrien.com>
2023-06-14 19:15:27 -0400

    Updated services and custom events

--- simple-application/delete-services.sh
+++ simple-application/delete-services.sh
@@ -5,7 +5,7 @@ kubectl delete  -f orange-service.yaml
 kubectl delete  -f green-service.yaml
 kubectl delete  -f blue-service.yaml
 
-kubectl delete -f ./namespace-limit-range.yaml --namespace=optimization-single-application
+kubectl delete -f ./namespace-limit-range.yaml --namespace=optimization-colors-applications
 
-kubectl delete ns optimization-single-application
+kubectl delete ns optimization-colors-applications
 
--- simple-application/run-simple-application.sh
+++ simple-application/run-simple-application.sh
@@ -1,13 +1,14 @@
-kubectl create ns optimization-single-application
+kubectl create ns optimization-colors-applications
 
-kubectl apply -f ./namespace-limit-range.yaml --namespace=optimization-single-application
+kubectl apply -f ./namespace-limit-range.yaml --namespace=optimization-colors-applications
 
-kubectl apply  -f simple-application.yaml
-kubectl apply  -f red-service.yaml
-kubectl apply  -f yellow-service.yaml
+#kubectl apply  -f simple-application.yaml
+kubectl apply  -f red-service.yaml; # stress --vm 2 --vm-bytes 50M --vm-hang 60s --backoff 10s --verbose;  memory: "55Mi"memory: "102Mi"
+kubectl apply  -f yellow-service.yaml; # stress --vm 2 --vm-bytes 50M --vm-hang 60s --backoff 10s --verbose; memory: "110Mi"memory: "200Mi"
 kubectl apply  -f orange-service.yaml
 kubectl apply  -f green-service.yaml
-kubectl apply  -f blue-service.yaml
+#kubectl apply  -f blue-service.yaml
+#kubectl apply  -f grey-service.yaml
 
 
 
