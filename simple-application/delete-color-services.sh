a2afcf5e8cc0d3b18ce0a087789fd46acea2ca9e
hughbrien <hugh@hughbrien.com>
2023-06-18 13:04:26 -0400

    Latest updates for Color Optimize Demo

--- simple-application/delete-color-services.sh
+++ simple-application/delete-color-services.sh
@@ -1,11 +1,11 @@
-kubectl delete  -f simple-application.yaml
 kubectl delete  -f red-service.yaml
 kubectl delete  -f yellow-service.yaml
 kubectl delete  -f orange-service.yaml
 kubectl delete  -f green-service.yaml
 kubectl delete  -f blue-service.yaml
+kubectl delete  -f grey-service.yaml
 
-kubectl delete -f ./namespace-limit-range.yaml --namespace=optimization-colors-applications
+kubectl delete -f ./namespace-limit-range.yaml --namespace=color-optimize
 
-kubectl delete ns optimization-colors-applications
+kubectl delete ns color-optimize
 
