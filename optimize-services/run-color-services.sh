880303c85e0206f87438eaf4a2137804f7dca065
hughbrien <hugh@hughbrien.com>
2023-06-19 15:47:56 -0400

    Updated Services

--- optimize-services/run-color-services.sh
+++ optimize-services/run-color-services.sh
@@ -2,7 +2,7 @@ kubectl create ns color-optimize
 
 #kubectl apply -f ./namespace-limit-range.yaml --namespace=color-optimize
 
-#kubectl apply  -f simple-application.yaml
+#kubectl apply  -f optimize-services.yaml
 kubectl apply  -f red-service.yaml; # stress --vm 2 --vm-bytes 50M --vm-hang 60s --backoff 10s --verbose;  memory: "55Mi"memory: "102Mi"
 kubectl apply  -f yellow-service.yaml; # stress --vm 2 --vm-bytes 50M --vm-hang 60s --backoff 10s --verbose; memory: "110Mi"memory: "200Mi"
 kubectl apply  -f orange-service.yaml
@@ -12,6 +12,6 @@ kubectl apply  -f blue-service.yaml
 
 
 
-#kubectl describe limits simple-application -- $NAMESPACE
+#kubectl describe limits optimize-services -- $NAMESPACE
 #kubectl delete -n $NAMESPACE -f https://raw.githubusercontent.com/komodorio/komodor-tutorials/master/failure-scenarios/application-error-with-exception/simple-application.yaml
 #kubectl delete ns $NAMESPACE
