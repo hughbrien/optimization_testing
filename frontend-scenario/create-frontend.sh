8bab278e2a76d7d8e8ad95e60dcfe427a8eea0ec
hughbrien <hugh@hughbrien.com>
2023-06-22 11:10:57 -0400

    moved directories to their own Git Repositories

--- frontend-scenario/create-frontend.sh
+++
@@ -1,8 +0,0 @@
-## Create Frontend Network Service Namespace and Deployment
-
-kubectl apply -f otimization-testing-namespace-service.yaml
-kubectl apply -f otimization-testing-config-map.yaml
-kubectl apply -f frontend.yaml
-kubectl apply -f catalog.yaml
-kubectl apply -f pricing.yaml
-kubectl apply -f shipping.yaml
--- frontend-scenario/delete-all.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete -f ./otimization-testing-namespace-service.yaml
-kubectl delete -f ./otimization-testing-config-map.yaml
-kubectl delete -f ./frontend.yaml
-kubectl delete -f ./catalog.yaml
-kubectl delete -f ./pricing.yaml
